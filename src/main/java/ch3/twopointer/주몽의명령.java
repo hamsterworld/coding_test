74d7f2de9b38b8856231e7a814767030efa2aa8c
hamster <hamster@hamster.com>
2022-11-13 15:39:54 +0900

    주몽의망령

--- src/main/java/ch3/twopointer/주몽의명령.java
+++ src/main/java/ch3/twopointer/주몽의명령.java
@@ -1,11 +1,14 @@
+package ch3.twopointer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class 주묭의명령 {
-    public static void main(String[] args) throws IOException{
+public class 주몽의명령 {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -13,26 +16,29 @@ public class 주묭의명령 {
         int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         int[] list = new int[N];
+
         for(int i = 0;i<N;i++){
             list[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(list);
         int count = 0;
-        int i =0;
-        int j = N - 1;
-        while(i < j){
-            if(list[i] + list[j] < M){
-                i++;
-            } else if (list[i]+list[j] > M){
-                j--;
-            } else {
-                count ++;
-                i++;
-                j--;
-            }
+        int i=0;
+        int j=N-1;
 
+
+        while( i < j ){
+            if( (list[i]+list[j]) < M ){
+                i++;
+            } else if((list[i]+list[j]) > M ){
+                j--;
+            } else if((list[i]+list[j]) == M){
+                j--;
+                count++;
+            }
         }
 
         System.out.println(count);
+
     }
 }
