e010ae5c7edcf13c8ca7e1f15b1376cf31f877ca
hamster <hamster@hamster.com>
2022-11-13 15:02:59 +0900

    연속된자ì연수의합구하기

---
+++ src/main/java/ch3/twopointer/연속된자연수의합구하기.java
@@ -0,0 +1,38 @@
+package ch3.twopointer;
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class 연속된자연수의합구하기 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(i != end_index){
+            if( i == sum){
+                count ++;
+                end_index++;
+                sum = sum + end_index;
+            } else if( i > sum){
+                end_index++;
+                sum = sum + end_index;
+            } else if( i < sum){
+                sum = sum - start_index;
+                start_index++;
+            }
+        }
+
+        System.out.println(count);
+
+
+    }
+
+}
